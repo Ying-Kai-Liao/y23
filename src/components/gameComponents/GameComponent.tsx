3d10e60873087a14b7423f13c59b7d0abb9926cd
YKL0919 <kevin@startluo.com>
2023-08-29 22:32:19 +0800

    2023.08.29-4

--- src/components/gameComponents/GameComponent.tsx
+++ src/components/gameComponents/GameComponent.tsx
@@ -144,8 +144,8 @@ const GameComponent = ({ questions, totalQuestions }: Props) => {
                         <a href="/" className="absolute top-[3vw] right-[1vw] w-20 md:top-[4vw] md:right-[1vw] md:w-40">
                             <img src="/image/game/button/back_button.png"></img>
                         </a>
-                        <img src="/image/lil_banner6.png" className="max-w-[200px] md:max-w-[300px] m-6 ml-[22vw] lg:mt-[15vw] xl:mt-[6vw]"></img>
-                        <span className="text-lg md:text-2xl md:mb-30 lg:mb-[10vw] xl:mb-[10vw] px-[70px] text-center ">
+                        <img src="/image/lil_banner6.png" className="absolute max-w-[200px] md:max-w-[300px] m-6 mb-[106vw] ml-[22vw] lg:mt-[15vw] xl:mt-[6vw]"></img>
+                        <span className="absolute text-lg md:text-2xl mb-[92vw] md:mb-30 lg:mb-[10vw] xl:mb-[10vw] px-[70px] text-center ">
                             自我們成立以來，已經服務了超過
                             <VisibilitySensor onChange={onVisibilityChange}>
                                 {({ isVisible }: { isVisible: boolean }) => (
@@ -156,7 +156,7 @@ const GameComponent = ({ questions, totalQuestions }: Props) => {
                             </VisibilitySensor>
                             人。
                         </span>
-                        <div className="flex flex-col text-sm md:text-lg mb-[185vw] text-white text-center items-center">
+                        <div className="flex flex-col text-sm md:text-lg mb-[10vw] text-white text-center items-center">
                             {isMobile
                                 ? (
                                     <span className="font-semibold">
@@ -171,17 +171,14 @@ const GameComponent = ({ questions, totalQuestions }: Props) => {
                                     </span>
                                 )
                             }
-                            <img src="/image/banner-overlay-img-about-word.png" className="max-w-[250px] mt-5 mb-5"></img>
+                            <img src="/image/banner-overlay-img-about-word.png" className="max-w-[230px] mt-4 mb-4"></img>
                             {isMobile
                                 ? (
                                     <span className="font-semibold">
                                         學會正確地投資和管理財務，<br />
                                         從而穩健地營造自己的生活。<br />
-                                        <br />
                                         我們鼓勵您訂閱我們的雜誌，<br />
                                         以獲得最新的理財資訊、專家觀點和投資建議。<br />
-                                        這將是您獲取有關投資、財務管理<br />
-                                        和理財幸福的寶貴資源。<br /><br />
                                         謝謝您的支持，我們期待與您一起建立一個<br />
                                         理財、理善、理健康的全民理財幸福島！
                                     </span>
@@ -189,7 +186,6 @@ const GameComponent = ({ questions, totalQuestions }: Props) => {
                                     <span>
                                         學會正確地投資和管理財務，從而穩健地營造自己的生活。<br />
                                         我們鼓勵您訂閱我們的雜誌，以獲得最新的理財資訊、專家觀點和投資建議。<br />
-                                        這將是您獲取有關投資、財務管理和理財幸福的寶貴資源。<br />
                                         謝謝您的支持，我們期待與您一起建立一個理財、理善、理健康的全民理財幸福島！
                                     </span>
                                 )
@@ -197,16 +193,16 @@ const GameComponent = ({ questions, totalQuestions }: Props) => {
                             <a href="https://pse.is/56zyed" target="_blank" className="absolute top-[120vw] w-[150px] md:top-[208vw] md:right-[56vw] md:w-40">
                                 <img src="/image/button.png"></img>
                             </a>
-                            <a href="https://pse.is/56s6tl" target="_blank" className="absolute top-[166vw] right-[60vw] w-[38px] md:top-[206.5vw] md:right-[39vw] md:w-9">
+                            <a href="https://pse.is/587egy" target="_blank" className="absolute top-[166vw] right-[60vw] w-[38px] md:top-[206.5vw] md:right-[39vw] md:w-9">
                                 <img src="/image/icon-FB.png"></img>
                             </a>
-                            <a href="https://pse.is/58h69p" target="_blank" className="absolute top-[166vw] w-[38px] md:top-[206.5vw] md:right-[34vw] md:w-9">
+                            <a href="https://pse.is/58en9p" target="_blank" className="absolute top-[166vw] w-[38px] md:top-[206.5vw] md:right-[34vw] md:w-9">
                                 <img src="/image/icon-ig.png"></img>
                             </a>
                             <a href="https://lin.ee/C5fnsgJ" target="_blank" className="absolute top-[166vw] right-[30vw] w-[38px] md:top-[206.5vw] md:right-[29vw] md:w-9">
                                 <img src="/image/icon-line.png"></img>
                             </a>
-                            <a href="https://www.moneyweekly.com.tw/ArticleData/Info/Article/127224" target="_blank" className="absolute top-[266vw] w-[150px] md:top-[240.5vw] md:right-[42vw] md:w-40">
+                            <a href="https://www.moneyweekly.com.tw/ArticleData/Info/Article/127224" target="_blank" className="absolute top-[266.5vw] w-[138px] md:top-[240.5vw] md:right-[42vw] md:w-40">
                                 <img src="/image/game/button/go_button.png"></img>
                             </a>
                             <a href="https://www.moneyweekly.com.tw/Magazine/Info/%E7%90%86%E8%B2%A1%E5%91%A8%E5%88%8A/38582" target="_blank" className="absolute top-[310vw] w-[250px] md:top-[259vw] md:w-70">
